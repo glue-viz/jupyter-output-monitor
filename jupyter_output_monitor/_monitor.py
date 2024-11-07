5903f997ab71bd3fb61c6512a8831d15a6a1cafd
Brett M. Morris <bmmorris@stsci.edu>
2024-11-07 15:15:27 -0500

    mac OS-friendly isotime paths

--- jupyter_output_monitor/_monitor.py
+++ jupyter_output_monitor/_monitor.py
@@ -18,6 +18,10 @@ from ._utils import clear_notebook, isotime
 RG_SPECIAL = (143, 56)
 
 
+def iso_to_path(time):
+    return time.replace(':', '-')
+
+
 @click.command()
 @click.option(
     "--notebook",
@@ -42,7 +46,7 @@ RG_SPECIAL = (143, 56)
 @click.option("--headless", is_flag=True, help="Whether to run in headless mode")
 def monitor(notebook, url, output, wait_after_execute, headless):
     if output is None:
-        output = f"output-{isotime()}"
+        output = f'output-{iso_to_path(isotime())}'
 
     if os.path.exists(output):
         print(f"Output directory {output} already exists")
@@ -124,12 +128,9 @@ def _monitor_output(url, output, wait_after_execute, headless):
 
             timestamp = isotime()
 
-            # Colons are invalid in filenames on Windows
-            filename_timestamp = timestamp.replace(":", "-")
-
             screenshot_filename = os.path.join(
                 output,
-                f"input-{input_index:03d}-{filename_timestamp}.png",
+                f"input-{input_index:03d}-{iso_to_path(timestamp)}.png",
             )
             image = Image.open(BytesIO(screenshot_bytes))
             image.save(screenshot_filename)
@@ -192,12 +193,9 @@ def _monitor_output(url, output, wait_after_execute, headless):
 
                         timestamp = isotime()
 
-                        # Colons are invalid in filenames on Windows
-                        filename_timestamp = timestamp.replace(":", "-")
-
                         screenshot_filename = os.path.join(
                             output,
-                            f"output-{output_index:03d}-{filename_timestamp}.png",
+                            f"output-{output_index:03d}-{iso_to_path(timestamp)}.png",
                         )
                         image = Image.open(BytesIO(screenshot_bytes))
                         image.save(screenshot_filename)
